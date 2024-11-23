7e3b74d3eea1cb6975935a852044e35555794e59
Africa-t0t0 <jorge.ramirezz@mail.udp.cl>
2024-11-22 22:03:16 -0300

    understanding context and structure

--- src/store/shopping-cart-context.jsx
+++ src/store/shopping-cart-context.jsx
@@ -1,7 +1,9 @@
 import { createContext } from "react";
 
 
+// by using the () => {} we are indicating that it can receive a function.
+
 export const CartContext = createContext({
     items: [],
-
+    addItemToCart: () => {}
 });
